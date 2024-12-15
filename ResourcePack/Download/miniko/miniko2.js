2bcec927e2ce51a098b312a985824362d7d252e3
NonKun Ch <116896756+NonKun1122@users.noreply.github.com>
2024-12-15 10:29:22 +0700

    Add files via upload

---
+++ ResourcePack/Download/miniko/miniko2.js
@@ -0,0 +1,8 @@
+function downloadFile() {
+    const link = document.createElement('a');
+    link.href = 'MiNiKoUi-V2.mcpack'; // ใส่ URL ของไฟล์ที่ต้องการดาวน์โหลด
+    link.download = 'MiNiKoUi-V2.mcpack'; // ชื่อไฟล์ที่จะถูกดาวน์โหลด
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
