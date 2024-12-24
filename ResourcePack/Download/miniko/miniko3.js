7323e4977320e60c61d1238c5d7b2abcdc86a8e7
NonKun Ch <116896756+NonKun1122@users.noreply.github.com>
2024-12-24 11:36:12 +0700

    Update miniko3.js

--- ResourcePack/Download/miniko/miniko3.js
+++ ResourcePack/Download/miniko/miniko3.js
@@ -1,7 +1,7 @@
 function downloadFile() {
     const link = document.createElement('a');
-    link.href = 'MiNiKoUi-V2.mcpack'; // ใส่ URL ของไฟล์ที่ต้องการดาวน์โหลด
-    link.download = 'MiNiKoUi-V2.mcpack'; // ชื่อไฟล์ที่จะถูกดาวน์โหลด
+    link.href = 'MiNiKoUi-V2.1.mcpack'; // ใส่ URL ของไฟล์ที่ต้องการดาวน์โหลด
+    link.download = 'MiNiKoUi-V2.1.mcpack'; // ชื่อไฟล์ที่จะถูกดาวน์โหลด
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
